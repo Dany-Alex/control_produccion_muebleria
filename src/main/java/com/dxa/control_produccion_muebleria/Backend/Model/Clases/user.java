dcd021966e598db06579dab8b624c3b9d33fc72a
Daniel Estuardo <dany.phone7@gmail.com>
2021-08-11 15:27:18 -0600

    creacion de la clase de usuario

---
+++ src/main/java/com/dxa/control_produccion_muebleria/Backend/Model/Clases/user.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dxa.control_produccion_muebleria.Backend.Model.Clases;
+
+/**
+ *
+ * @author Artist
+ */
+public class user {
+
+    private String name, password;
+    private int type;
+
+    public user(String name, String password, int type) {
+        this.name = name;
+        this.password = password;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+}
